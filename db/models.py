21420a71f8ce6700c8ab5bf053f827455cb2107f
Daniel Oravec <danoravec@gmail.com>
2020-12-05 22:02:50 +0100

    db schema, not controllers yet

--- db/models.py
+++ db/models.py
@@ -1,14 +1,19 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, TIMESTAMP, DateTime, CheckConstraint, Time, Date
 from sqlalchemy.orm import relationship
+from datetime import datetime
 
-from .database import Base
+from .database import Base, utcnow
 
 
 class User(Base):
     __tablename__ = 'users'
 
-    id = Column(String, primary_key=True, index=True)
-    active = Column(Boolean, default=True, nullable=False)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    card = Column(String, nullable=False)
+    first_name = Column(String)
+    last_name = Column(String)
+    created = Column(DateTime, server_default=utcnow())
+    updated = Column(DateTime, server_default=utcnow(), onupdate=utcnow())  # TODO Check whether onupdates is correct
 
     groups = relationship('UserGroup')
 
@@ -16,8 +21,10 @@ class User(Base):
 class Group(Base):
     __tablename__ = 'groups'
 
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, index=True, unique=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String, index=True, unique=True)
+    created = Column(DateTime, server_default=utcnow())
+    updated = Column(DateTime, server_default=utcnow(), onupdate=utcnow())
 
     rules = relationship('GroupRule')
 
@@ -25,9 +32,14 @@ class Group(Base):
 class Rule(Base):
     __tablename__ = 'rules'
 
-    id = Column(Integer, primary_key=True, index=True)
-    title = Column(String, nullable=True)
-    # TODO add time interval
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
+    name = Column(String, nullable=True)
+    allow = Column(Boolean, server_default="False", nullable=False)
+    # access_point_id = Column(Integer, ...)
+    # time_spec_id = Column(Integer, ...
+    priority = Column('priority', Integer, CheckConstraint('0 <= priority AND priority <= 10'), nullable=False, server_default='5')
+    created = Column(DateTime, server_default=utcnow())
+    updated = Column(DateTime, server_default=utcnow(), onupdate=utcnow())
 
     groups = relationship('GroupRule')
 
@@ -35,14 +47,32 @@ class Rule(Base):
 class UserGroup(Base):
     __tablename__ = 'user_group'
 
-    id = Column(Integer, primary_key=True)
-    user_id = Column(String, ForeignKey('users.id'))
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('users.id'))
     group_id = Column(Integer, ForeignKey('groups.id'))
+    created = Column(DateTime, server_default=utcnow())
+    updated = Column(DateTime, server_default=utcnow(), onupdate=utcnow())
 
 
 class GroupRule(Base):
     __tablename__ = 'group_rule'
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     group_id = Column(Integer, ForeignKey('groups.id'))
     rule_id = Column(Integer, ForeignKey('rules.id'))
+    created = Column(DateTime, server_default=utcnow())
+    updated = Column(DateTime, server_default=utcnow(), onupdate=utcnow())
+
+
+class TimeSpec(Base):
+    __tablename__ = 'time_spec'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String, server_default='rule')
+    weekday_mask = Column('weekday_mask', Integer, CheckConstraint('0 <= weekday_mask AND weekday_mask <= 255'), nullable=False)
+    time_from = Column(Time, nullable=False)
+    time_to = Column(Time, nullable=False)
+    date_from = Column(Date, nullable=False, server_default=utcnow())
+    date_to = Column(Date, nullable=False)  # TODO server_default?
+    created = Column(DateTime, server_default=utcnow())
+    updated = Column(DateTime, server_default=utcnow(), onupdate=utcnow())
