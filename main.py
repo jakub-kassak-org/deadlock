acdb43bfd82926d985051f005004af1619c5e5c2
Daniel Oravec <danoravec@gmail.com>
2021-06-09 17:48:57 +0200

    [GET] /groups/{group_id}/ bugfix: returning empty list when no users in group instead of null

--- main.py
+++ main.py
@@ -197,8 +197,6 @@ async def get_groups(offset: int = 0, limit: int = 100, db: Session = Depends(ge
 @app.get("/groups/{group_id}/")
 async def get_group(group_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_active_user)):
     users_in_group = crud.get_users_from_group(db=db, group_id=group_id)
-    if not users_in_group:
-        raise HTTPException(status_code=400, detail=f"Group with id {group_id} was not found.")
     return {
         'group_id': group_id,
         'users': users_in_group
